6e823da31124d4d4720d464405cfe6930b19f561
Eric MARTIN <eric.martin85@gmail.com>
2017-05-17 16:01:26 +0200

    first commit

---
+++ src/ProductBundle/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ProductBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    // src/AppBundle/Controller/SecurityController.php
+
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
